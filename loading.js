2624239f04a33dfefa3c7ef1550d1a6e15f21dc4
Manuel Cruz <mancruz@publicisgroupe.net>
2019-04-21 21:37:03 -0600

    updated loading logo in css

---
+++ loading.js
@@ -0,0 +1,15 @@
+
+
+
+  window.addEventListener ('load', function(){
+    loader.style.borderRadius= "50%";
+    loader.style.height= '190px';
+    loader.style.width= '190px';
+    loader.style.visibility= 'hidden';
+});
+
+var logo = document.getElementById('logo');
+  window.addEventListener ('load', function(){
+    logo.style.borderRadius= "50%";
+    logo.style.visibility= 'hidden';
+});
